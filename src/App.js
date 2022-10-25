cf96c64bf0a0d82b6fd83174448abab94160c1d3
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-25 22:01:11 +0300

    adding not-found page && create route

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import {
   BrowserRouter,
   Routes, // instead of "Switch"
   Route,
+  Navigate,
 } from "react-router-dom";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-        hello
+
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,58 @@
 import React from 'react'
-
-export const Navbar = () => {
+import {Link} from "react-router-dom"
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <div className="navbar bg-black bg-opacity-40">
+        <div className="flex-1">
+          <Link to="/">
+            <img
+              src="https://images.unsplash.com/photo-1602934445884-da0fa1c9d3b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1916&q=80"
+              width="50"
+              height="42"
+              alt=""
+              className="ml-2"
+            />
+          </Link>
+ 
+        </div>
+     
+
+          <Link to="/cart">
+           <div className="flex-none p-1">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-lg indicator-item text-red-700 text-2xl ">
+                  5
+                </span>
+              </div>
+            </label>
+            <div
+              tabIndex={0}
+              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+            ></div>
+          </div>
+        </div> 
+          </Link>
+        
+      </div>
+    </div>
+  );
 }
+
+export default Navbar
