063b0bb8a58bf500923666dcefc0956c63d7306a
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-26 22:42:23 +0300

    Adding cardsDetail page (design)

--- src/components/CardsDetail.js
+++ src/components/CardsDetail.js
@@ -1,9 +1,147 @@
 import React from 'react'
-
+import {BsTrash} from "react-icons/bs";
 const CardsDetail = () => {
   return (
-    <div>CardsDetail</div>
-  )
+    <div>
+      <p className="text-center p-3 text-3xl">Item Detail Page</p>
+      <section>
+        <div class="relative mx-auto max-w-screen-xl px-4 py-8">
+          <div class="grid grid-cols-1 items-start gap-8 md:grid-cols-2">
+            <div class="grid grid-cols-1 gap-4">
+              <img
+                alt="Les Paul"
+                src="https://images.unsplash.com/photo-1456948927036-ad533e53865c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                class=" aspect-square w-full rounded-xl object-cover"
+              />
+            </div>
+
+            <div class="sticky top-0">
+              <div class="flex justify-between">
+                <div class="text-white max-w-[35ch]">
+                  <h1 class="text-2xl font-bold">
+                    Name: Fun Product That Does Something Cool
+                  </h1>
+
+                  <p class="mt-0.5 text-sm">Category : Highest Rated Product</p>
+
+                  <div class="mt-2 -ml-0.5 flex">
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg
+                      class="h-5 w-5 text-gray-200"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+                </div>
+
+                <p class="text-lg font-bold text-white">$119.99</p>
+              </div>
+
+              <details class="group relative mt-4">
+                <summary class="block">
+                  <div>
+                    <div class="prose max-w-none group-open:hidden">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ipsa veniam dicta beatae eos ex error culpa delectus rem
+                        tenetur, architecto quam nesciunt, dolor veritatis nisi
+                        minus inventore, rerum at recusandae?
+                      </p>
+                    </div>
+
+                    <span class="mt-4 cursor-pointer text-sm font-medium underline group-open:absolute group-open:bottom-0 group-open:left-0 group-open:mt-0">
+                      Read More
+                    </span>
+                  </div>
+                </summary>
+
+                <div class="prose max-w-none pb-6">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsa veniam dicta beatae eos ex error culpa delectus rem
+                    tenetur, architecto quam nesciunt, dolor veritatis nisi
+                    minus inventore, rerum at recusandae?
+                  </p>
+
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Placeat nam sapiente nobis ea veritatis error consequatur
+                    nisi exercitationem iure laudantium culpa, animi temporibus
+                    non! Maxime et quisquam amet. A, deserunt!
+                  </p>
+                </div>
+              </details>
+
+              <form class="mt-8  ">
+                <div class="mt-4 flex justify-between">
+                  <p class="mb-1 text-lg font-bold text-red-600">
+                    Total : $234.45
+                  </p>
+
+                  <button>
+                    <BsTrash
+                      size={25}
+                      className="text-red-600 hover:text-red-700"
+                    />
+                  </button> 
+                </div>
+
+
+           <div class="border m-4 flex justify-center w-1/5">
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+            <input class="mx-2 border text-center w-8" type="text" value="1" />
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+          </div>
+
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default CardsDetail
